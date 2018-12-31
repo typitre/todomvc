2d28fb13a3e799d1cf437965503408a7c35f8cfd
typitre <tyrell.pitre@gmail.com>
2018-12-31 08:49:33 -0700

    Completion of Beast 2

--- examples/jquery/js/app.js
+++ examples/jquery/js/app.js
@@ -48,6 +48,7 @@ jQuery(function ($) {
 			new Router({
 				'/:filter': function (filter) {
 					this.filter = filter;
+					this.save();
 					this.render();
 				}.bind(this)
 			}).init('/all');
@@ -63,6 +64,9 @@ jQuery(function ($) {
 				.on('focusout', '.edit', this.update.bind(this))
 				.on('click', '.destroy', this.destroy.bind(this));
 		},
+		save: function() {
+			util.store('todos-jquery', this.todos);
+		},
 		render: function () {
 			var todos = this.getFilteredTodos();
 			$('#todo-list').html(this.todoTemplate(todos));
@@ -70,7 +74,6 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -91,6 +94,7 @@ jQuery(function ($) {
 				todo.completed = isChecked;
 			});
 
+			this.save();
 			this.render();
 		},
 		getActiveTodos: function () {
@@ -117,6 +121,7 @@ jQuery(function ($) {
 		destroyCompleted: function () {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
+			this.save();
 			this.render();
 		},
 		// accepts an element from inside the `.item` div and
@@ -148,11 +153,13 @@ jQuery(function ($) {
 
 			$input.val('');
 
+			this.save();
 			this.render();
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
+			this.save();
 			this.render();
 		},
 		edit: function (e) {
@@ -184,10 +191,12 @@ jQuery(function ($) {
 				this.todos[this.indexFromEl(el)].title = val;
 			}
 
+			this.save();
 			this.render();
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
+			this.save();
 			this.render();
 		}
 	};
